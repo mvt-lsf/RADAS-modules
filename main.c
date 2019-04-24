ad9c931d5d3d6dd2080c339fcbb037ecd054d3c2
sanflan <sandalinasfrancisco@gmail.com>
2019-04-24 10:29:29 -0300

    Funcionan correctamente los callbacks

--- main.c
+++ main.c
@@ -15,13 +15,18 @@ struct callback_data{
 	I16 *channel1_buffer;
 };
 
-void callback(struct callback_data data){
-	printf("callback");
-	int buffersize = data.config->bins;
-	//I16 *buffer = data->channel0_buffer;
-	//for(int i = 0; i < buffersize; i++){
-	//	printf("voltage: %d", buffer[i]);
-	//}
+struct callback_data *callback_data;
+
+void callback(void *data){
+	printf("\ncallback");
+	printf(" %d ", callback_data->cardnumber);
+	I16 *buffer = callback_data->channel0_buffer;
+	int buffersize = callback_data->config->bins;
+	double voltage;
+	for(int i = 0; i < buffersize; i++){
+		WD_AI_VoltScale(callback_data->cardnumber, AD_B_2_V, buffer[i], &voltage);
+		printf(" voltage: %f", voltage);
+	}
 	//hacer algo con los buffers
 	Sleep(100);
 	//FILE *file = fopen("file","a");
@@ -135,7 +140,6 @@ void read_config(char * filename, struct config *config){
 
 int main(){
 	struct config *config;
-	struct callback_data *callback_data;
 	int pointsperchunk;
 	int buffersize;
 	I16 Id;
@@ -164,8 +168,8 @@ int main(){
 	printf("\nBuffers setup");
 
 	printf("\nTrying to setup callback");
-	I16 call = WD_AI_EventCallBackEx_x64(cardnumber, 1, TrigEvent, (ULONG_PTR) callback, callback_data);
-	//I16 call = WD_AI_EventCallBack_x64(cardnumber, 1, TrigEvent, (ULONG_PTR)callback);
+	//I16 call = WD_AI_EventCallBackEx_x64(cardnumber, 1, TrigEvent, (ULONG_PTR) callback, callback_data);
+	I16 call = WD_AI_EventCallBack_x64(cardnumber, 1, TrigEvent, (ULONG_PTR)callback);
 	printf("\nCallback registered %d", call);
 	
 	err = WD_AI_ContReadChannel(cardnumber, 0, 0, config->bins, 1, 1, ASYNCH_OP);
