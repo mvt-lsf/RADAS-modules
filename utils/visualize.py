eb492627fcd1dc6ea309b92aa9cfd1f00ede593b
sanflan <sandalinasfrancisco@gmail.com>
2019-04-26 12:22:20 -0300

    prueba de visualize

--- utils/visualize.py
+++ utils/visualize.py
@@ -3,7 +3,6 @@ import numpy
 import matplotlib.pyplot
 #import bottleneck
 
-
 handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             win32file.GENERIC_READ,
                             0,
@@ -12,15 +11,20 @@ handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             0,
                             None)
 print(handle)
+readsize = 3000 * 50 * 8	#toma un chunk
+samples = 1					#cantidad de chunks que queremos
 while True:
-	readsize = 3000*5000
+	print(readsize)
 	err,data = win32file.ReadFile(handle, readsize)
 	buffer = data
-	#while len(data) == readsize:
-	#	err,data = win32file.ReadFile(handle, readsize)
-	#	buffer += data
+	for i in range(samples-1):
+		err,data = win32file.ReadFile(handle, readsize)
+		buffer += data
 	buffer = numpy.frombuffer(buffer, dtype=numpy.float64)
+	print(buffer.shape)
+	buffer.shape = (3000,-1)
 #	matplotlib.pyplot.plot(waterfall)
 #	matplotlib.pyplot.figure()
 #	matplotlib.pyplot.imshow(waterfall, aspect='auto', cmap='jet')  # plot con el filtro
-	matplotlib.pyplot.plot(buffer)
+	matplotlib.pyplot.figure()
+	matplotlib.pyplot.imshow(buffer, aspect='auto', cmap='jet')
