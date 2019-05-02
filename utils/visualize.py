43ee3b2df6535038a736d3adeac04c3b0e5d4304
sanflan <sandalinasfrancisco@gmail.com>
2019-05-02 09:59:20 -0300

    colgue con que era esto

--- utils/visualize.py
+++ utils/visualize.py
@@ -11,20 +11,23 @@ handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             0,
                             None)
 print(handle)
-readsize = 3000 * 50 * 8	#toma un chunk
-samples = 1					#cantidad de chunks que queremos
+readsize = 3000 * 10 * 8	#toma un chunk
+#samples = 1					#cantidad de chunks que queremos
 while True:
 	print(readsize)
 	err,data = win32file.ReadFile(handle, readsize)
 	buffer = data
-	for i in range(samples-1):
-		err,data = win32file.ReadFile(handle, readsize)
-		buffer += data
+#	for i in range(samples-1):
+#		err,data = win32file.ReadFile(handle, readsize)
+#		buffer += data
 	buffer = numpy.frombuffer(buffer, dtype=numpy.float64)
 	print(buffer.shape)
 	buffer.shape = (3000,-1)
+	matplotlib.pyplot.plot(buffer)
+	matplotlib.pyplot.show()
+
 #	matplotlib.pyplot.plot(waterfall)
 #	matplotlib.pyplot.figure()
 #	matplotlib.pyplot.imshow(waterfall, aspect='auto', cmap='jet')  # plot con el filtro
-	matplotlib.pyplot.figure()
-	matplotlib.pyplot.imshow(buffer, aspect='auto', cmap='jet')
+#	matplotlib.pyplot.figure()
+#	matplotlib.pyplot.imshow(buffer, aspect='auto', cmap='jet')
