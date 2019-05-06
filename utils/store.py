56a5d8d78adaaf7c291e513ccad9a7691c7f1eb7
sanflan <sandalinasfrancisco@gmail.com>
2019-05-06 11:51:11 -0300

    store ahora con pickle

--- utils/store.py
+++ utils/store.py
@@ -1,6 +1,7 @@
 import win32pipe, win32file
-import resource
+import datetime
 import sys
+import pickle
 
 handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             win32file.GENERIC_READ,
@@ -9,9 +10,10 @@ handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             win32file.OPEN_EXISTING,
                             0,
                             None)
-output_file = open(sys.argv[1],'w')
-readsize = resource.getpagesize()
+output_file = open(sys.argv[1],"wb")
+readsize = 4096
 
 while True:
         err,data = win32file.ReadFile(handle, readsize)
-        file.write(data, file=output_file)
+        pickle.dump(datetime.datetime.now(), output_file)
+        pickle.dump(data, output_file)
