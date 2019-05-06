1fce4103250acd810d283d209cdf18902549e54a
sanflan <sandalinasfrancisco@gmail.com>
2019-05-06 14:54:06 -0300

    almacena 10 minutos y borra

--- utils/store.py
+++ utils/store.py
@@ -1,5 +1,6 @@
 import win32pipe, win32file
 import datetime
+import os
 import sys
 import pickle
 
@@ -10,7 +11,7 @@ handle = win32file.CreateFile("\\\\.\\pipe\\ch0_raw_pipe",
                             win32file.OPEN_EXISTING,
                             0,
                             None)
-output_file = open(sys.argv[1],"wb")
+output_file = open(sys.argv[1] + "/data","wb")
 readsize = 4096
 time = datetime.datetime.now()
 while True:
@@ -18,4 +19,8 @@ while True:
         err,data = win32file.ReadFile(handle, readsize)
         pickle.dump(now, output_file)
         pickle.dump(data, output_file)
-        print(time.minute - now.minute)
+        if (now.minute - time.minute) <= 5 :
+                output_file.close()
+                os.remove(sys.argv[1] + "tmp_file")
+                os.rename(sys.argv[1] + "/data", sys.argv[1] + "tmp_file")
+                output_file = open(sys.argv[1] + "/data","wb")
